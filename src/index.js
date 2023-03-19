55c95496d22534da9b9df5b05cedb65471d1a595
Shubham <shubhamkahar.ele16@gmail.com>
2023-03-19 11:44:33 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM  from "react-dom/client";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Booklist = () => {
+ return (
+  <section className="booklist">
+   return <h2 style={{ color: 'Yellow', fontSize: '4rem', marginTop: '.5rem' , padding: '0 rem'}}>Book Lists</h2>;
+   <Book />
+   <Book />
+   <Book />
+   <Book />
+   <Book />
+   <Book />
+  </section>
+ );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+ return <article className="book">
+  
+  <Image />
+  <Title />
+  <Author />
+ </article>
+}
+
+
+const Image = () => <img src = './images/book.jpg' alt ="Atomic Habits" />;
+const Title = () => <h2>Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones</h2>;
+const Author = () => {
+ return <h4>James Clear </h4>;
+};
+
+
+const root= ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Booklist />);
